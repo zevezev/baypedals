44147909a036f44f078ce61c7da3cac5127530ff
Zev <zev_izenberg@brown.edu>
2022-06-16 14:18:00 -0700

    new mutation

---
+++ convex/deleteAllPosts.ts
@@ -0,0 +1,9 @@
+//CAREFUL WITH THIS FUNCTION
+import { mutation } from "convex-dev/server";
+import { Id } from "convex-dev/values";
+
+// Send a chat message.
+export default mutation(({ db }, body: ArrayBuffer) => {
+  const post = { body, time: Date.now() };
+  db.insert("posts", post);
+});
